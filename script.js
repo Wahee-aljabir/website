7119fcd76783fa9586c4378f40a24211dc04af27
Wahee-aljabir <waheealjabir14@gmail.com>
2024-06-15 21:12:16 +0100

    header

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function scrollToSection(sectionClass) {
     document.querySelector('.' + sectionClass).scrollIntoView({ behavior: 'smooth' });
-}
+};
+
+$(document).ready(function(){
+
+    $('.buttons').hide()
+    $('.toggle').click(display)
+
+    var click = 0
+
+    function display () {
+        $(".buttons").show('slow')
+        click = click + 1
+
+        if (click === 1)
+            $(".buttons").show('slow')
+
+        if (click === 2)
+            $(".buttons").hide('slow')
+
+        if (click > 2)
+            click = 1
+        console.log(click)
+    };
+});
