b95c010641cd0445a303a5e2033c4ca2062f1074
Wahee-aljabir <waheealjabir14@gmail.com>
2024-06-29 13:30:59 +0100

    geography questions

--- Quiz.js
+++ Quiz.js
@@ -12,7 +12,7 @@ const questions = [
         question: "Which country is considered the most powerful in terms of military strength?",
         answers: [
             { text: "United States", correct: true},
-            { text: "Russia", correct: false},
+            { text: "Russia", correct: true},
             { text: "China", correct: false},
             { text: "Germany", correct: false},
         ]
@@ -34,7 +34,196 @@ const questions = [
             { text: "China", correct: true},
             { text: "Brazil", correct: false},
         ]
-    }
+    },
+    {
+        question: "Capital of France?",
+        answers: [
+            { text: "Paris", correct: true},
+            { text: "Kuala Lumpur", correct: false},
+            { text: "Madrid", correct: false},
+            { text: "Cairo", correct: false},
+        ]
+    },
+    {
+        question: "Capital of UK?",
+        answers: [
+            { text: "Andorra la Vella", correct: false},
+            { text: "London", correct: true},
+            { text: "Kabul", correct: false},
+            { text: "Algiers", correct: false},
+        ]
+    },
+    {
+        question: "Capital of USA?",
+        answers: [
+            { text: "Indonesia", correct: false},
+            { text: "Washington D.C", correct: true},
+            { text: "Abu Dhabi", correct: false},
+            { text: "Vatican City", correct: false},
+        ]
+    },
+    {
+        question: "Capital of Russia?",
+        answers: [
+            { text: "Cornwall", correct: false},
+            { text: "Bristol", correct: false},
+            { text: "Moscow", correct: true},
+            { text: "Leeds", correct: false},
+        ]
+    },
+    {
+        question: "Main Religion of Palestine in 1920s?",
+        answers: [
+            { text: "Islam", correct: true},
+            { text: "Chistianity", correct: false},
+            { text: "Juedism", correct: false},
+            { text: "Zionism", correct: false},
+        ]
+    },
+    {
+        question: "Capital Of Australia?",
+        answers: [
+            { text: "Canberra", correct: true},
+            { text: "Bourmemouth", correct: false},
+            { text: "Nottingham", correct: false},
+            { text: "Plymouth", correct: false},
+        ]
+    },
+    {
+        question: "Capital Of India?",
+        answers: [
+            { text: "New Delhi", correct: true},
+            { text: "Bourmemouth", correct: false},
+            { text: "Nottingham", correct: false},
+            { text: "Sheffield", correct: false},
+        ]
+    },
+    {
+        question: "Richest Country?",
+        answers: [
+            { text: "UK", correct: false},
+            { text: "U.S.A", correct: true},
+            { text: "China", correct: false},
+            { text: "Russia", correct: false},
+        ]
+    },
+    {
+        question: "Precise population of the world in 2024?",
+        answers: [
+            { text: "10B", correct: false},
+            { text: "6.7B", correct: false},
+            { text: "8B", correct: false},
+            { text: "7.8B", correct: true},
+        ]
+    },
+    {
+        question: "Smallest Country?",
+        answers: [
+            { text: "Bahrain", correct: false},
+            { text: "Vatican City", correct: true},
+            { text: "Monaco", correct: false},
+            { text: "Nauru", correct: false},
+        ]
+    },
+    {
+        question: "Capital of South Africa?",
+        answers: [
+            { text: "Cape Town", correct: true},
+            { text: "Kinshasa", correct: false},
+            { text: "Brighton", correct: false},
+            { text: "Lyme Regis", correct: false},
+        ]
+    },
+    {
+        question: "Capital of Japan in 1942?",
+        answers: [
+            { text: "Nagoya", correct: false},
+            { text: "Kyoto", correct: false},
+            { text: "Harrogate", correct: false},
+            { text: "Tokyo", correct: true},
+        ]
+    },
+    {
+        question: "Capital of Pakistan?",
+        answers: [
+            { text: "Faisalabad", correct: false},
+            { text: "Islamabad", correct: true},
+            { text: "Multan", correct: false},
+            { text: "Lahore", correct: false},
+        ]
+    },
+    {
+        question: "Capital of Algeria?",
+        answers: [
+            { text: "Sidi be Abbes", correct: false},
+            { text: "Oran", correct: false},
+            { text: "Algiers", correct: true},
+            { text: "Coach Constantine", correct: false},
+        ]
+    },
+    {
+        question: "Capital of Bangladesh?",
+        answers: [
+            { text: "Dhaka", correct: true},
+            { text: "Sylhet", correct: false},
+            { text: "Khulna", correct: false},
+            { text: "Rangpur", correct: false},
+        ]
+    },
+    {
+        question: "Enemy of North Korea?",
+        answers: [
+            { text: "Everyone", correct: true},
+            { text: "Korea", correct: false},
+            { text: "South Korea", correct: false},
+            { text: "None", correct: false},
+        ]
+    },
+    {
+        question: "Capital of North Korea?",
+        answers: [
+            { text: "Wonsan", correct: false},
+            { text: "Nampo", correct: false},
+            { text: "Hamhung", correct: false},
+            { text: "PyongYang", correct: true},
+        ]
+    },
+    {
+        question: "Where is EU headquarters?",
+        answers: [
+            { text: "Coventry", correct: false},
+            { text: "Chester", correct: false},
+            { text: "Belgium", correct: true},
+            { text: "Brussels", correct: true},
+        ]
+    },
+    {
+        question: "Capital of Morroco?",
+        answers: [
+            { text: "Rabat", correct: true},
+            { text: "Exeter", correct: false},
+            { text: "Anglesey", correct: false},
+            { text: "Keswick", correct: false},
+        ]
+    },
+    {
+        question: "Capital of Morroco?",
+        answers: [
+            { text: "Rabat", correct: true},
+            { text: "Exeter", correct: false},
+            { text: "Anglesey", correct: false},
+            { text: "Keswick", correct: false},
+        ]
+    },
+    {
+        question: "Nuclear Nations",
+        answers: [
+            { text: "UK", correct: true},
+            { text: "North Korea", correct: true},
+            { text: "India", correct: true},
+            { text: "Pakistan", correct: true},
+        ]
+    },
 ];
 
 const questionElement = document.getElementById("question");
